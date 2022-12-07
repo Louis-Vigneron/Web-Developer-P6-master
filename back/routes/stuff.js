8457b5e2ac6b32df14c049d7776305f8a0a45bc5
Louis.V <loulou38101@gmail.com>
2022-12-07 16:10:59 +0100

    fix error server start

--- back/routes/stuff.js
+++ back/routes/stuff.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
 
 router.post('/', stuffCtrl.createThing);
-router.put('/:id', stuffCtrl.modifydThing);
+router.put('/:id', stuffCtrl.modifyThing);
 router.delete('/:id', stuffCtrl.deleteThing);
 router.get('/:id',stuffCtrl.getOneThing);
 router.get('/', stuffCtrl.getAllThing);
