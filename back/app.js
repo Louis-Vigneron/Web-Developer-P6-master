6d492f933d926de2dcfb1123086df341e85f0ffa
Louis.V <loulou38101@gmail.com>
2022-12-21 15:30:51 +0100

    Add like/dislikes

--- back/app.js
+++ back/app.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 
-const stuffRoutes = require('./routes/stuff');
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://Louis:1234@cluster0.ozbeyw8.mongodb.net/?retryWrites=true&w=majority',
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/api/sauces', stuffRoutes);
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
---
+++ back/controllers/sauce.js
@@ -0,0 +1,127 @@
+const Sauce = require('../models/sauce');
+const fs = require('fs');
+const sauce = require('../models/sauce');
+
+exports.createSauce = (req, res, next) => {
+
+    const sauce = JSON.parse(req.body.sauce);
+    delete sauce._id;
+
+    const { userId, name, manufacturer, description, mainPepper, heat } = sauce;
+    console.log(sauce);
+
+    const Newsauce = new Sauce({
+
+        userId,
+        name,
+        manufacturer,
+        description,
+        mainPepper,
+        heat,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: [],
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    Newsauce.save()
+        .then(() => { res.status(201).json({ message: 'Sauce enregistré !' }) })
+        .catch(error => { res.status(400).json({ error }) })
+};
+
+exports.modifySauce = (req, res, next) => {
+    const sauceObject =  req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  
+    delete sauceObject._userId;
+    Sauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            if (sauce.userId != req.auth.userId) {
+                res.status(401).json({ message : 'Not authorized'});
+            } else {
+                Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
+                .then(() => res.status(200).json({message : 'Sauce modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }})
+        .catch((error) => {res.status(400).json({ error })});
+ };
+
+ exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id})
+        .then(sauce => {
+            if (sauce.userId != req.auth.userId) {
+                res.status(401).json({message: 'Non autorisé !'});
+            } else {
+                const filename = sauce.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Sauce.deleteOne({_id: req.params.id})
+                        .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                        .catch(error => res.status(401).json({ error }));
+                });
+            }
+        })
+        .catch( error => {
+            res.status(500).json({ error });
+        });
+ };
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
+}
+
+exports.getAllSauce = (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.likeSauce = (req, res, next) => {
+    const {like, userId} = req.body;
+
+    Sauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            if (like === 1) 
+            {
+                res.status(200).json(sauce);
+                sauce.usersLiked.push(userId);
+                sauce.likes = sauce.usersLiked.length;
+                console.log(sauce);
+                sauce.save()
+            } 
+            if (like ===-1) 
+            {
+               
+                res.status(200).json({message : 'I hate !'});
+                sauce.usersDisliked.push(userId);
+                sauce.dislikes = sauce.usersDisliked.length;
+                console.log(sauce);
+                sauce.save()
+            }
+            if (like === 0)
+            {
+                res.status(200).json({message : 'Neutral'})
+                if (userId.includes(sauce.usersLiked))
+                {
+                    sauce.usersLiked = sauce.usersLiked.filter(user => user !==userId)
+                    sauce.likes = sauce.usersLiked.length;
+                }
+                if(userId.includes(sauce.usersDisliked))
+                {
+                    sauce.usersDisliked = sauce.usersDisliked.filter(user => user !==userId)
+                    sauce.dislikes = sauce.usersDisliked.length;
+                }
+                console.log(sauce);
+                sauce.save()
+            }
+            
+        
+        })
+        .catch((error) => {res.status(400).json({ error })});
+    
+    
+
+}
